271eeb869fbf57cdd50f12206bf500dd54006b2c
Brendan <brenkao@gmail.com>
2017-03-15 15:39:56 -0700

    jason's initial design

--- app/src/main/java/com/example/jason/fooder1/LoginPage.java
+++ app/src/main/java/com/example/jason/fooder1/LoginPage.java
@@ -1,15 +1,14 @@
 package com.example.jason.fooder1;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.Auth;
@@ -21,38 +20,20 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 /**
  * Created by Jason on 3/15/2017.
- * Modified by Jake 4/24/2017
  */
 
 public class LoginPage extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
-    private static final String TAG = "GoogleActivity";
-    private static final int RC_SIGN_IN = 9001;
-
     private LinearLayout Prof_Section;
+    private Button SignOut;
     private SignInButton SignIn;
-    public static TextView Name, Email;
+    private TextView Name, Email;
     private ImageView Prof_Pic;
     private GoogleApiClient googleApiClient;
-
-    // [START declare_auth]
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    // [END declare_auth]
-
-    private GoogleApiClient mGoogleApiClient;
-    private TextView mStatusTextView;
-    private TextView mDetailTextView;
+    private static final int REQ_CODE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,213 +41,99 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
         setContentView(R.layout.login_screen);
 
         Prof_Section = (LinearLayout)findViewById(R.id.prof_section);
+        SignOut = (Button)findViewById(R.id.bn_logout);
         SignIn = (SignInButton)findViewById(R.id.bn_login);
         Name = (TextView)findViewById(R.id.name);
         Email = (TextView)findViewById(R.id.email);
         Prof_Pic = (ImageView)findViewById(R.id.prof_pic);
         SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
         Prof_Section.setVisibility(View.GONE);
-        // Views
-        //mStatusTextView = (TextView) findViewById(R.id.status);
-        //mDetailTextView = (TextView) findViewById(R.id.detail);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(firebaseAuth.getCurrentUser() != null) {
-                    startActivity(new Intent(LoginPage.this, MainActivity.class));
-                }
-            }
-        };
-        // Button listeners
-        findViewById(R.id.bn_login).setOnClickListener(this);
-        //findViewById(R.id.disconnect_button).setOnClickListener(this);
-
-        // [START config_signin]
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        // [END config_signin]
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-        // [START initialize_auth]
-        mAuth = FirebaseAuth.getInstance();
-        // [END initialize_auth]
     }
-
-    // [START on_start_check_user]
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        mAuth.addAuthStateListener(mAuthListener);
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-    // [END on_start_check_user]
-
-    // [START onactivityresult]
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-                handleResult(result);
-            } else {
-                // Google Sign In failed, update UI appropriately
-                // [START_EXCLUDE]
-                updateUI(null);
-                // [END_EXCLUDE]
-            }
-        }
-    }
-    // [END onactivityresult]
-
-    // [START auth_with_google]
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-        // [START_EXCLUDE silent]
-        //showProgressDialog();
-        // [END_EXCLUDE]
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(LoginPage.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-                        // [START_EXCLUDE]
-                        //hideProgressDialog();
-                        // [END_EXCLUDE]
-                    }
-                });
-    }
-    // [END auth_with_google]
-
-    // [START signin]
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-
-    }
-    // [END signin]
-
-    private void signOut() {
-        // Firebase sign out
-        mAuth.signOut();
-
-        // Google sign out
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(@NonNull Status status) {
-                        updateUI(null);
-                    }
-                });
-    }
-
-    private void revokeAccess() {
-        // Firebase sign out
-        mAuth.signOut();
-
-        // Google revoke access
-        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(@NonNull Status status) {
-                        updateUI(null);
-                    }
-                });
-    }
-
-    private void updateUI(FirebaseUser user) {
-        //hideProgressDialog();
-        if (user != null) {
-            //mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
-            //mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
-
-            //findViewById(R.id.bn_login).setVisibility(View.GONE);
-            //findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
-        } else {
-            //mStatusTextView.setText(R.string.signed_out);
-            //mDetailTextView.setText(null);
-
-            //findViewById(R.id.bn_login).setVisibility(View.VISIBLE);
-            //findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.bn_login:
+                signIn();
+                break;
+            case R.id.bn_logout:
+                signOut();
+                break;
         }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
-        // be available.
-        Log.d(TAG, "onConnectionFailed:" + connectionResult);
-        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+
     }
 
-    private void handleResult(GoogleSignInResult result) {
+    private void signIn()
+    {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent, REQ_CODE);
 
-        String name = null;
-        String email = null;
-
-        if(result.isSuccess()) {
-            GoogleSignInAccount account = result.getSignInAccount();
-            name = account.getDisplayName();
-            email = account.getEmail();
-            if (account.getPhotoUrl() != null) {
-                String img_url = account.getPhotoUrl().toString();
-                Glide.with(this).load(img_url).into(Prof_Pic);
+    }
+    private void signOut()
+    {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
             }
+        });
+
+    }
+    private void handleResult(GoogleSignInResult result)
+    {
+        if(result.isSuccess())
+        {
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            String img_url = account.getPhotoUrl().toString();
             Name.setText(name);
             Email.setText(email);
-
+            Glide.with(this).load(img_url).into(Prof_Pic);
             //updateUI(true);
-
-        } else {
-            //updateUI(false);
+            startActivity(new Intent(LoginPage.this, MainActivity.class));
         }
 
-        Intent intent = new Intent(LoginPage.this, MainActivity.class);
-        Bundle info = new Bundle();
-        info.putString("myName", name);
-        info.putString("myEmail", email);
-        intent.putExtras(info); //Put your id to your next Intent
-        startActivity(intent);
+        else
+        {
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isLogin)
+    {
+        if (isLogin)
+        {
+            Prof_Section.setVisibility(View.VISIBLE);
+            SignIn.setVisibility(View.GONE);
+
+        }
+        else
+        {
+            Prof_Section.setVisibility(View.GONE);
+            SignIn.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
-    public void onClick(View v) {
-        int i = v.getId();
-        if (i == R.id.bn_login) {
-            signIn();
-        } /**
-        if (i == R.id.bn_login) {
-            signOut();
-            revokeAccess();
-        }**/
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQ_CODE)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
     }
+
+
 }
--- app/src/main/java/com/example/jason/fooder1/TinderCard.java
+++ app/src/main/java/com/example/jason/fooder1/TinderCard.java
@@ -7,7 +7,9 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
+import com.mindorks.placeholderview.annotations.Click;
 import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
 import com.mindorks.placeholderview.annotations.Resolve;
 import com.mindorks.placeholderview.annotations.View;
 import com.mindorks.placeholderview.annotations.swipe.SwipeCancelState;
@@ -15,9 +17,6 @@ import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
 import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * Created by Jason on 3/14/2017.
@@ -35,44 +34,27 @@ public class TinderCard {
     @View(R.id.locationNameTxt)
     private TextView locationNameTxt;
 
-    private String mBusiness;
+    private Profile mProfile;
     private Context mContext;
     private SwipePlaceHolderView mSwipeView;
-    private String business;
-    private JSONObject mBusiness2;
-    private JSONObject getAddress;
-    private static JSONObject getCoord;
 
-    public TinderCard(Context context, String bus, SwipePlaceHolderView swipeView) throws JSONException {
+    public TinderCard(Context context, Profile profile, SwipePlaceHolderView swipeView) {
         mContext = context;
-        mBusiness = bus;
+        mProfile = profile;
         mSwipeView = swipeView;
-
-        mBusiness2 = new JSONObject(mBusiness);
-        String getLocation = mBusiness2.getString("location");
-        getAddress = new JSONObject(getLocation);
-        String getCoordinates = mBusiness2.getString("coordinates");
-        getCoord = new JSONObject((getCoordinates));
-
     }
 
     @Resolve
-    private void onResolved() throws JSONException{
-
-
-        Glide.with(mContext).load(mBusiness2.getString("image_url")).into(profileImageView);
-        namePriceTxt.setText(mBusiness2.getString("name") + ", " + mBusiness2.getString("price"));
-        locationNameTxt.setText(getAddress.getString("display_address"));
-
-    }
-    public String getName() throws JSONException
-    {
-        return mBusiness2.getString("name");
+    private void onResolved(){
+        Glide.with(mContext).load(mProfile.getImageUrl()).into(profileImageView);
+        namePriceTxt.setText(mProfile.getName() + ", " + mProfile.getPrice());
+        locationNameTxt.setText(mProfile.getLocation());
     }
 
-    @SwipeIn
-    private void onSwipeIn() {
-        Log.d("EVENT", "onSwipedIn");
+    @SwipeOut
+    private void onSwipedOut(){
+        Log.d("EVENT", "onSwipedOut");
+        mSwipeView.addView(this);
     }
 
     @SwipeCancelState
@@ -80,8 +62,11 @@ public class TinderCard {
         Log.d("EVENT", "onSwipeCancelState");
     }
 
-    @SwipeOut
-    private void onSwipedOut(){ Log.d("EVENT", "onSwipedOut"); }
+    @SwipeIn
+    private void onSwipeIn(){
+        Log.d("EVENT", "onSwipedIn");
+    }
+
     @SwipeInState
     private void onSwipeInState(){
         Log.d("EVENT", "onSwipeInState");
@@ -90,16 +75,5 @@ public class TinderCard {
     @SwipeOutState
     private void onSwipeOutState(){
         Log.d("EVENT", "onSwipeOutState");
-    }
-
-    public static double getLongitude() throws JSONException{
-        return getCoord.getDouble("longitude");
-    }
-    public static double getLatitude() throws JSONException{
-        return getCoord.getDouble("latitude");
-    }
-    public String getID() throws JSONException
-    {
-        return mBusiness2.getString("id");
     }
 }
